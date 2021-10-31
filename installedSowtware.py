99a9b486f03ccd8df780559921475e99db91281d
shimon <shimon.on5@gmail.com>
2021-10-31 14:39:06 +0200

    some improvments

--- installedSowtware.py
+++ installedSowtware.py
@@ -1,7 +1,34 @@
 import winreg
-from winreg import QueryValueEx
+from winreg import QueryValueEx, EnumKey
 
-reg = winreg.ConnectRegistry(None, winreg.HKEY_LOCAL_MACHINE)
-k = winreg.OpenKeyEx(reg, r'SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall\{090DCBF9-8353-4E9D-B155-337B2A324D1B}')
-val = QueryValueEx(k, "DisplayName")
-print(val)
+
+def connect_to_registry(key):
+    reg = winreg.ConnectRegistry(None, key)
+    return reg
+
+
+def get_sw_lst_key(reg_conn):
+    k = winreg.OpenKeyEx(reg_conn, r'SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall')
+    return k
+
+
+def get_sw_lst():
+    aKey = r"SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall"
+    aReg = connect_to_registry(winreg.HKEY_LOCAL_MACHINE)
+    print(r"*** Reading from %s ***" % aKey)
+    aKey = winreg.OpenKey(aReg, aKey)
+    requested_data_field = "DisplayName"  # choose here which field you need
+    sw_lst = []
+    for i in range(1024):
+        try:
+            asubkey_name = EnumKey(aKey, i)
+            asubkey = winreg.OpenKey(aKey, asubkey_name)
+            val = QueryValueEx(asubkey, requested_data_field)
+            sw_lst.append(val)
+            print(val)
+        except FileNotFoundError:
+            continue
+        except EnvironmentError:
+            print(r"*** %s files was found ***" % i)
+            break
+        return sw_lst
