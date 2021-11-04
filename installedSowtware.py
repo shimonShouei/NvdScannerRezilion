909c3dc3e9344d3d15857dd55cdb4a72ad4d5750
Shaya Omer <os15587@gmail.com>
2021-11-04 14:34:27 +0200

    name changes complete 2

--- installedSowtware.py
+++ installedSowtware.py
@@ -7,32 +7,26 @@ def connect_to_registry(key):
     reg = winreg.ConnectRegistry(None, key)
     return reg
 
-
-def get_sw_lst_key(reg_conn):
-    k = winreg.OpenKeyEx(reg_conn, r'SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall')
-    return k
-
-
-def get_sw_lst(aRegHK):
+def get_software_lst(MainKey):
     aKey = r"SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall"
     aReg = connect_to_registry(MainKey)
     print(r"*** Reading from %s %s ***" % (key_dict[MainKey], aKey))
     aKey = winreg.OpenKey(aReg, aKey)
     requested_data_field = "DisplayName"  # choose here which field you need
-    sw_lst = []
+    software_lst = []
     for i in range(1024):
         try:
             asubkey_name = EnumKey(aKey, i)
             asubkey = winreg.OpenKey(aKey, asubkey_name)
             val = QueryValueEx(asubkey, requested_data_field)
-            sw_lst.append(val)
+            software_lst.append(val)
             print(val[0])
         except FileNotFoundError:
             continue
         except EnvironmentError:
             print(r"*** %s files was found ***" % i)
             break
-    return sw_lst
+    return software_lst
 
 
 if __name__ == "__main__":
