3f162f628dcaedb4aa5a1989dfa6a402fd4d50f9
Shaya Omer <os15587@gmail.com>
2021-11-21 13:22:38 +0200

    made basic xml parser class

--- xmlParser.py
+++ xmlParser.py
@@ -1,36 +1,66 @@
-# import xml.etree.ElementTree as ET
+import xml.etree.ElementTree as et
+# import pandas as pd
+
+# namespaces = {'': 'http://cpe.mitre.org/dictionary/2.0',
+#                   'xsi': "http://www.w3.org/2001/XMLSchema-instance",
+#                   'scap-core': "http://scap.nist.gov/schema/scap-core/0.3",
+#                   'cpe-23': "http://scap.nist.gov/schema/cpe-extension/2.3",
+#                   "ns6": "http://scap.nist.gov/schema/scap-core/0.1",
+#                   'meta': "http://scap.nist.gov/schema/cpe-dictionary-metadata/0.2"}
+# df = pd.read_xml('official-cpe-dictionary_v2.3TESTS.xml', xpath='//cpe-23:cpe23-item', namespaces=namespaces)
+# splited = df['name'].str.split(':', n=12, expand=True)
+# new_splited_columns = ['cpe', 'cpe_version', 'part', 'vendor', 'product', 'version', 'update', 'edition', 'language',
+#                        'sw_edition', 'target_sw', 'target_hw', 'other']
+# df[new_splited_columns] = splited
+# df = df.drop(['deprecation', 'cpe'], axis=1)
+# # df.to_csv('official-cpe-dictionary_v2.3.csv')
 #
-# tree = ET.parse('official-cpe-dictionary_v2.3.xml')
-# root = tree.getroot()
-# elemList = []
-# for elem in root.iter():
-#     elemList.append(elem.tag)
-# # now I remove duplicities - by convertion to set and back to list
-# elemList = list(set(elemList))
-# # Just printing out the result
-# print(elemList)
-import pandas as pd
-namespaces = {'xsi':"http://www.w3.org/2001/XMLSchema-instance",
-              'scap-core':"http://scap.nist.gov/schema/scap-core/0.3",
-              'cpe-23': "http://scap.nist.gov/schema/cpe-extension/2.3",
-              "ns6":"http://scap.nist.gov/schema/scap-core/0.1",
-              'meta':"http://scap.nist.gov/schema/cpe-dictionary-metadata/0.2",
-              'schemaLocation':"http://scap.nist.gov/schema/cpe-extension/2.3"
-                               " https://scap.nist.gov/schema/cpe/2.3/cpe-dictionary-extension_2.3.xsd"
-                               " http://cpe.mitre.org/dictionary/2.0"
-                               " https://scap.nist.gov/schema/cpe/2.3/cpe-dictionary_2.3.xsd"
-                               "http://scap.nist.gov/schema/cpe-dictionary-metadata/0.2"
-                               " https://scap.nist.gov/schema/cpe/2.1/cpe-dictionary-metadata_0.2.xsd"
-                               " http://scap.nist.gov/schema/scap-core/0.3"
-                               " https://scap.nist.gov/schema/nvd/scap-core_0.3.xsd"
-                               " http://scap.nist.gov/schema/configuration/0.1"
-                               " https://scap.nist.gov/schema/nvd/configuration_0.1.xsd"
-                               " http://scap.nist.gov/schema/scap-core/0.1"
-                               " https://scap.nist.gov/schema/nvd/scap-core_0.1.xsd"}
-df = pd.read_xml('official-cpe-dictionary_v2.3.xml', xpath='//cpe-23:cpe23-item', namespaces=namespaces)
-splited = df['name'].str.split(':', n=12, expand=True)
-new_splited_columns = ['cpe', 'cpe_version', 'part', 'vendor', 'product', 'version', 'update', 'edition', 'language',
-                       'sw_edition', 'target_sw', 'target_hw', 'other']
-df[new_splited_columns] = splited
-df = df.drop(['deprecation', 'cpe'], axis=1)
-df.to_csv('official-cpe-dictionary_v2.3.csv')
+# x = df[df['product'] == 'form_maker']
+# print(x)
+
+
+class CpeXml:
+    namespaces = {'': 'http://cpe.mitre.org/dictionary/2.0',
+                  'xsi': "http://www.w3.org/2001/XMLSchema-instance",
+                  'scap-core': "http://scap.nist.gov/schema/scap-core/0.3",
+                  'cpe-23': "http://scap.nist.gov/schema/cpe-extension/2.3",
+                  "ns6": "http://scap.nist.gov/schema/scap-core/0.1",
+                  'meta': "http://scap.nist.gov/schema/cpe-dictionary-metadata/0.2"}
+    file_name = 'official-cpe-dictionary_v2.3TESTS.xml'
+
+    def __init__(self, file_name):
+        self.tree = et.parse(file_name)
+        self.root = self.tree.getroot()
+
+    def get_all_cpe_items(self):
+        return self.root.findall('cpe-item', self.namespaces)
+
+    def get_all_cpe_items_names(self):
+        cpe_names = []
+        for cpe_item in self.get_all_cpe_items():
+            cpe_names.append(cpe_item.attrib.get('name'))
+
+    def parse_cpe_name(self, cpe_item):
+        return cpe_item.str.split(':')
+
+    def get_all_titles_text(self):
+        titles_text = []
+        for cpeItem in self.get_all_cpe_items():
+            titles_text.append(cpeItem.find('title', self.namespaces).text)
+        return titles_text
+
+    def string_to_cpe(self, prog_name):
+        for cpe_item in self.get_all_cpe_items():
+            if prog_name in cpe_item.find('title', self.namespaces).text:
+                return cpe_item
+
+
+
+
+
+
+
+
+
+
+
