dc285943e96b76913f737e0d9b963388f9fa2d7c
liel binyamin <lielbin@post.bgu.ac.il>
2021-11-21 19:30:51 +0200

    get list of files of program files

--- main.py
+++ main.py
@@ -1,13 +1,22 @@
+import get_files_programfiles
 from cve_parser import CveParser
 
 
 def main():
-    parse = CveParser()
-    year = "2021"
-    cve_specific_year_collection = parse.get_cve_collection_for_specific_year(year)
-    print(cve_specific_year_collection[0].identifier)
-    print(cve_specific_year_collection[0].cve_to_string())
-    parse.write_all_cve_collection_for_specific_year_to_file("2002")
+    # parse = CveParser()
+    # year = "2021"
+    # cve_specific_year_collection = parse.get_cve_collection_for_specific_year(year)
+    # print(cve_specific_year_collection[0].identifier)
+    # print(cve_specific_year_collection[0].cve_to_string())
+    # parse.write_all_cve_collection_for_specific_year_to_file("2002")
+
+    list_of_path_directory =  []
+    list_of_path_files = []
+    get_files_programfiles.get_file_from_path_by_dfs(list_of_path_directory, list_of_path_files, "C:\Program Files")
+    get_files_programfiles.get_file_from_path_by_dfs(list_of_path_directory, list_of_path_files, "C:\Program Files (x86)")
+    print(list_of_path_files)
+    # get_files_programfiles.get_file_from_path("C:\Program Files (x86)")
+
 
 
 
