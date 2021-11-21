a274a5458ee4beca8b2298d38ee3faf1d9a42709
liel binyamin <lielbin@post.bgu.ac.il>
2021-11-21 20:48:46 +0200

    get list of files of program files and program files X86

--- get_files_programfiles.py
+++ get_files_programfiles.py
@@ -28,27 +28,3 @@ def get_file_from_path_by_dfs(list_of_path_directory, list_of_path_files, path):
     else:
         pass
 
-# def get_file_from_path(path):
-#     my_list = os.listdir(path)
-#     list_of_path_directory = []
-#     only_files = []
-#     help_recursive_function(path, list_of_path_directory, only_files, my_list)
-#     # print(list_of_path_directory)
-#     print(only_files)
-
-# def help_recursive_function(path, list_of_path_directory, only_files, all_the_folder_in_this_path):
-#     if len(all_the_folder_in_this_path) == 0:
-#         return
-#     # check if thia path is directory
-#     var = path + "\\" + all_the_folder_in_this_path[0]
-#
-#     if os.path.isdir(var):
-#         list_of_path_directory.append(var)
-#         my_list = os.listdir(var)
-#         all_the_folder_in_this_path[0]
-#         help_recursive_function(var, list_of_path_directory, only_files, my_list)
-#     # check if thia path is file
-#     if os.path.isfile(var):
-#         only_files.append(var)
-#     else:
-#         return
