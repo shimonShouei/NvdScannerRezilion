838ebc978491d24ed458a77ee85a29c0783e4519
liel binyamin <lielbin@post.bgu.ac.il>
2021-12-05 11:16:59 +0200

    get list of files of software from program files and program files X86 The input name software contains in the name software in ProgramFiles

--- get_files_programfiles.py
+++ get_files_programfiles.py
@@ -7,10 +7,15 @@ import shutil
 def get_file_from_path(name_software):
     list_of_path_directory = []
     list_of_path_files = []
+    path_ = ["C:\Program Files\\", "C:\Program Files (x86)\\"]
+    for i in path_:
+        for neighbour in os.listdir(i):
+            if name_software.__contains__(neighbour):
+                name_software = neighbour
     path = ["C:\Program Files\\"+name_software, "C:\Program Files (x86)\\"+name_software]
     for i in path:
         get_file_from_path_by_dfs(list_of_path_directory, list_of_path_files, i)
-        print(list_of_path_files)
+    print(list_of_path_files)
     return list_of_path_files
 
 
